69f9611fba71b022732315ccbfb4a2f4b457b897
tony <chenyukang259@qbitnetwork.com>
2026-06-26 16:11:46 +0800

    fx __init__

--- agent/__init__.py
+++ agent/__init__.py
@@ -2,4 +2,4 @@ from .err_logs_agent import run_agent
 from .claude_code_agent import run_claude_code_agent
 from .log import log_handler
 
-__all__ = ["run_claude_code_agent", "run_agent", "run_jenkins_agent"]
+__all__ = ["run_claude_code_agent", "run_agent"]
